e6b7bc6e9f00010cc0406a5d249596c806a34a66
onlyonepen <kritramet1234@gmail.com>
2025-10-31 10:55:01 +1100

    made radio interactable

---
+++ Assets/_Script/Environment/ObjectBehaviour/RadioObject.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class RadioObject : MonoBehaviour, IInteractable
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] AudioClip switchSound;
+    bool isPlaying;
+
+    private void Start()
+    {
+        isPlaying = Random.Range(0f, 1f) > 0.5f;
+    }
+    public void OnInteracted()
+    {
+        isPlaying = !isPlaying;
+
+        AudioPoolManager.instance.PlayClipAtPoint(switchSound, transform.position);
+
+        if (audioSource != null)
+        {
+            audioSource.enabled = isPlaying;
+            Debug.Log("radio is playing " +  isPlaying);
+        }
+    }
+}
